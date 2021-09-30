9b36db8c93fc219719b3fcc3214019c16bb83997
callmetan <87515235+callmetan7@users.noreply.github.com>
2021-09-30 10:30:55 +0800

    Initial Commit

---
+++ src/com/tanujpadaliya/Main.java
@@ -0,0 +1,14 @@
+package com.tanujpadaliya;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Name: ");
+        String age = scanner.nextLine().trim();
+        System.out.println(age);
+    }
+
+}
